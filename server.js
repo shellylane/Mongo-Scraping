aa5c1c64b2830135e7731adf8493df11b73589bf
Artemis <35303907+slane1980@users.noreply.github.com>
2018-12-05 00:24:55 -0700

    update

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/MongoScraping");
+
+mongoose.connect(MONGODB_URI);
+
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Require the routes
+require("./routes/routes")(app);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
